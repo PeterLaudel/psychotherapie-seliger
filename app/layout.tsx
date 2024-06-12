ebe8f7def3aeced2ddfdb9d8138e3222cce83be4
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2024-06-12 20:20:46 +0200

    Improve Metadata for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,33 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Psychotherapie Seliger",
-  description: "Psychologische Psychotherapeutin in Leipzig",
+  title: "Psychologische Psychotherapeutin | Verhaltentherapie | Ute Seliger",
+  description:
+    "Psychotherapie (Verhaltenstherapie) in Leipzig/Waldstraßenviertel für Selbstzahler und Privatversicherte",
+  keywords: [
+    "Psychotherapie",
+    "Psychologische Psychotherapeutin",
+    "Ute Seliger",
+    "Psychotherapie Seliger",
+    "Praxis für Psychotherapie",
+    "Verhaltenstherapie",
+    "VT",
+    "Leipzig",
+    "Waldstraßenviertel",
+    "Selbstzahler",
+    "Privatversicherte",
+    "Depression",
+    "Angst",
+    "Zwang",
+    "Essstörung",
+    "Trauma",
+    "Burnout",
+    "Stress",
+    "Schlafstörung",
+    "Schmerz",
+    "Sucht",
+    "Krisenintervention",
+  ],
 };
 
 export default function RootLayout({
@@ -15,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="de">
       <body className={inter.className}>{children}</body>
     </html>
   );
