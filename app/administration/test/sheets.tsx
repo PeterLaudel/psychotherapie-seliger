3d9469433563c71b1982bf0b5d9bc8465c874ca9
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2024-10-17 17:14:30 +0200

    Add patient repository

--- app/administration/test/sheets.tsx
+++ app/administration/test/sheets.tsx
@@ -1,33 +1,9 @@
-import { getServerSession } from "next-auth";
-import { auth, sheets_v4 } from "@googleapis/sheets";
-import { authOptions } from "../../api/auth/[...nextauth]/config";
+import { getPatientRepository } from "../../../server";
+
 export default async function Sheets() {
-  const session = await getServerSession(authOptions);
+  const patientRepository = await getPatientRepository();
 
-  if (!session) {
-    return null;
-  }
-
-  const oauth2Client = new auth.OAuth2();
-  oauth2Client.setCredentials({
-    access_token: session?.accessToken,
+  return (await patientRepository.get()).map((patient) => {
+    return <div key={patient.id}>{`${patient.name} ${patient.surname}`}</div>;
   });
-
-  const sheetsClient = new sheets_v4.Sheets({
-    auth: oauth2Client,
-  });
-
-  const re = await sheetsClient.spreadsheets.get({
-    spreadsheetId: "1mJD8qEOMXNDKRmF1mNoaA68xcmFY1J7JbufKOA2-aOU",
-  });
-
-  return (
-    <div>
-      {re.data.sheets?.map((sheet) => {
-        return (
-          <div key={sheet.properties?.sheetId}>{sheet.properties?.title}</div>
-        );
-      })}
-    </div>
-  );
 }
---
+++ models/patient.ts
@@ -0,0 +1,10 @@
+export interface Patient {
+  id: string;
+  name: string;
+  surname: string;
+  email: string;
+  phone: string;
+  address: string;
+  zip: string;
+  city: string;
+}
