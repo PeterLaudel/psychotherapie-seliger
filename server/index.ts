36f6b9da5222736e4a601e3a242b9fc313eb2b6e
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2024-10-17 17:14:30 +0200

    Add patient repository

---
+++ server/index.ts
@@ -0,0 +1,29 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../app/api/auth/[...nextauth]/config";
+import { OAuth2Client } from "google-auth-library";
+import { people_v1 } from "@googleapis/people";
+import { PatientRepository } from "../repositories/patients";
+import { get } from "http";
+
+const getToken = async () => {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    throw new Error("No session found");
+  }
+  return session.accessToken;
+};
+
+const getAuthClient = async () => {
+  const token = await getToken();
+  const oAuthClient = new OAuth2Client();
+  oAuthClient.setCredentials({
+    access_token: token,
+  });
+  return oAuthClient;
+};
+
+export async function getPatientRepository() {
+  const auth = await getAuthClient();
+  const peopleClient = new people_v1.People({ auth });
+  return new PatientRepository(peopleClient);
+}
