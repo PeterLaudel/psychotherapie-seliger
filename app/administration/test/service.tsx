b67bb9145b1ff4ab206e7bedc03fdf27bfc7ffa3
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2024-10-19 22:41:23 +0200

    Set service

--- app/administration/test/service.tsx
+++ app/administration/test/service.tsx
@@ -1,6 +1,6 @@
 import { FieldArray } from "react-final-form-arrays";
 import { Service as ServiceType } from "../../../models/service";
-import { Field } from "react-final-form";
+import { Field, useField } from "react-final-form";
 import DatePicker from "react-datepicker";
 import CreatableSelect from "react-select/creatable";
 import { BillEntry } from "./formProps";
@@ -10,10 +10,37 @@ interface Props {
   services: ServiceType[];
 }
 
+function InvalidSubscription({
+  name,
+  children,
+}: {
+  name: string;
+  children: (invalid: boolean) => JSX.Element;
+}) {
+  const {
+    meta: { invalid },
+  } = useField(name, { subscription: { invalid: true } });
+  return children(!!invalid);
+}
+
+function ValueSubscription<T>({
+  name,
+  children,
+}: {
+  name: string;
+  children: (value: T | undefined) => JSX.Element;
+}) {
+  const {
+    input: { value },
+  } = useField<T>(name, { subscription: { value: true } });
+  if (!value) return children(undefined);
+  return children(value);
+}
+
 export default function Service({ services }: Props) {
-  const serviceOptions = services.map((service) => ({
-    label: service.originalGopNr,
-    value: service.originalGopNr,
+  const serviceOptions = services.map((service, index) => ({
+    label: service.short,
+    value: index,
     ...service,
   }));
 
@@ -26,6 +53,7 @@ export default function Service({ services }: Props) {
               key={`${name}.date`}
               name={`${name}.date`}
               type="input"
+              validate={(value) => (value ? undefined : "Required")}
             >
               {({ input: { onChange, value } }) => (
                 <DatePicker
@@ -39,6 +67,7 @@ export default function Service({ services }: Props) {
               key={`${name}.service`}
               name={`${name}.service`}
               type="select"
+              validate={(value) => (value ? undefined : "Required")}
             >
               {({ input }) => (
                 <CreatableSelect<ServiceType>
@@ -50,24 +79,48 @@ export default function Service({ services }: Props) {
                 />
               )}
             </Field>
-            <Field<number>
-              key={`${name}.number`}
-              name={`${name}.number`}
-              type="input"
-            >
-              {({ input }) => (
-                <input {...input} type="number" className="border-2" min={1} />
+            <InvalidSubscription name={`${name}.service`}>
+              {(invalid: boolean) => (
+                <Field<number>
+                  key={`${name}.number`}
+                  name={`${name}.number`}
+                  type="input"
+                >
+                  {({ input }) => (
+                    <input
+                      {...input}
+                      type="number"
+                      className="border-2"
+                      min={1}
+                      disabled={invalid}
+                    />
+                  )}
+                </Field>
               )}
-            </Field>
-            <Field<number>
-              key={`${name}.factor`}
-              name={`${name}.factor`}
-              type="input"
+            </InvalidSubscription>
+            <ValueSubscription<ServiceType | undefined>
+              name={`${name}.service`}
             >
-              {({ input }) => (
-                <input {...input} type="number" className="border-2" />
+              {(service) => (
+                <Field<number>
+                  key={`${name}.factor`}
+                  name={`${name}.factor`}
+                  type="select"
+                >
+                  {({ input }) => (
+                    <select {...input} className="border-2" disabled={!service}>
+                      {service?.amounts
+                        .filter((amount) => amount[1])
+                        .map((amount) => (
+                          <option key={amount[0]} value={amount[0]}>
+                            {amount[0]}
+                          </option>
+                        ))}
+                    </select>
+                  )}
+                </Field>
               )}
-            </Field>
+            </ValueSubscription>
           </Fragment>
         ))
       }
