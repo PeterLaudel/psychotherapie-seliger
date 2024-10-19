bd25f1eeab78b235dff4aaa20e2cab4a97c447d3
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2024-10-19 18:35:31 +0200

    Start to add form for bill creation

--- app/administration/test/sheets.tsx
+++ app/administration/test/sheets.tsx
@@ -1,17 +1,109 @@
 "use client";
 
 import { Patient } from "../../../models/patient";
-import Select from "react-select";
+import CreatableSelect from "react-select/creatable";
+import { Factor, Service } from "../../../models/service";
+import { Field, Form } from "react-final-form";
+import arrayMutators from "final-form-arrays";
+import { FieldArray } from "react-final-form-arrays";
+import { useMemo } from "react";
+
+interface BillEntry {
+  date?: Date;
+  service?: Service;
+  number?: number;
+  factor?: Factor;
+}
+
+interface FormProps {
+  patient?: Patient;
+  entries: BillEntry[];
+}
 
 interface Props {
   patients: Patient[];
+  services: Service[];
 }
 
-export default function Sheets({ patients }: Props) {
+export default function Sheets({ patients, services }: Props) {
   const options = patients.map((patient) => ({
     label: `${patient.name} ${patient.surname}`,
     value: patient.id,
+    ...patient,
   }));
 
-  return <Select options={options} />;
+  const serviceOptions = services.map((service) => ({
+    label: service.originalGopNr,
+    value: service.originalGopNr,
+    ...service,
+  }));
+
+  const initialValues = useMemo<FormProps>(
+    () => ({
+      entries: [
+        {
+          date: undefined,
+
+          service: undefined,
+          number: undefined,
+          factor: undefined,
+        },
+      ],
+    }),
+    []
+  );
+
+  return (
+    <div>
+      <Form<FormProps>
+        onSubmit={() => {}}
+        initialValues={initialValues}
+        mutators={{
+          // potentially other mutators could be merged here
+          ...arrayMutators,
+        }}
+      >
+        {({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <Field name="patient" type="select">
+              {({ input, meta: { touched, error } }) => (
+                <CreatableSelect<Patient>
+                  instanceId={input.name}
+                  {...input}
+                  options={options}
+                  isClearable={true}
+                  isValidNewOption={() => false}
+                />
+              )}
+            </Field>
+
+            <div>Leistungen</div>
+            <FieldArray name="entries">
+              {({ fields }) => (
+                <div>
+                  {fields.map((name) => (
+                    <Field
+                      key={`${name}.service`}
+                      name={`${name}.service`}
+                      type="select"
+                    >
+                      {({ input }) => (
+                        <CreatableSelect
+                          instanceId={input.name}
+                          {...input}
+                          options={serviceOptions}
+                          isClearable={true}
+                          isValidNewOption={() => false}
+                        />
+                      )}
+                    </Field>
+                  ))}
+                </div>
+              )}
+            </FieldArray>
+          </form>
+        )}
+      </Form>
+    </div>
+  );
 }
--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import { authOptions } from "../app/api/auth/[...nextauth]/config";
 import { OAuth2Client } from "google-auth-library";
 import { people_v1 } from "@googleapis/people";
 import { PatientRepository } from "../repositories/patients";
-import { get } from "http";
+import { ServiceRepository } from "../repositories/services";
 
 const getToken = async () => {
   const session = await getServerSession(authOptions);
@@ -26,4 +26,8 @@ export async function getPatientRepository() {
   const auth = await getAuthClient();
   const peopleClient = new people_v1.People({ auth });
   return new PatientRepository(peopleClient);
+}
+
+export async function getServicesRepository() {
+  return new ServiceRepository();
 }
