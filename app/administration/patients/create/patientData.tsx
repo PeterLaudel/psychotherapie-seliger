600c9b24110415cb82c7e4174095c8fdbd087814
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2024-11-23 14:01:11 +0100

    Add validation

--- app/administration/patients/create/patientData.tsx
+++ app/administration/patients/create/patientData.tsx
@@ -3,29 +3,63 @@ import Section from "../../../../components/section";
 import { TextField } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import dayjs from "dayjs";
+import {
+  validateDate,
+  validateEmail,
+  validateName,
+  validateSurname,
+} from "./validation";
 
 export default function PatientData() {
   return (
     <Section>
       <h2 className="mb-4">Patientendaten</h2>
       <div className="grid grid-cols-2 gap-4">
-        <Field name="name">
-          {({ input }) => <TextField {...input} label="Vorname" />}
+        <Field name="name" validate={validateName}>
+          {({ input, meta: { touched, error } }) => (
+            <TextField
+              {...input}
+              label="Vorname"
+              helperText={touched && error}
+              error={touched && !!error}
+            />
+          )}
         </Field>
-        <Field name="surname">
-          {({ input }) => <TextField {...input} label="Nachname" />}
+        <Field name="surname" validate={validateSurname}>
+          {({ input, meta: { touched, error } }) => (
+            <TextField
+              {...input}
+              label="Nachname"
+              helperText={touched && error}
+              error={touched && !!error}
+            />
+          )}
         </Field>
-        <Field name="email">
-          {({ input }) => <TextField {...input} label="E-Mail" />}
+        <Field name="email" validate={validateEmail}>
+          {({ input, meta: { touched, error } }) => (
+            <TextField
+              {...input}
+              label="E-Mail"
+              helperText={touched && error}
+              error={touched && !!error}
+            />
+          )}
         </Field>
-        <Field name="birthdate">
-          {({ input }) => (
+        <Field name="birthdate" validate={validateDate}>
+          {({ input, meta: { touched, error } }) => (
             <DatePicker
               {...input}
               label="Geburtsdatum"
               value={input.value ? dayjs(input.value) : null}
               onChange={(newValue) => input.onChange(newValue?.toDate())}
               minDate={dayjs.unix(0)}
+              slotProps={{
+                textField: {
+                  helperText: touched && error,
+                  error: touched && !!error,
+                  onBlur: input.onBlur,
+                },
+              }}
             />
           )}
         </Field>
