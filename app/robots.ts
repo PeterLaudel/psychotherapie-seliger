45e9860b79a3294a51179e7c76087686b1f2554e
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2024-12-01 13:31:17 +0100

    Fix typo

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
       disallow: [
         "/",
         "/administration/",
-        "/hom/datenschutz/",
+        "/home/datenschutz/",
         "/home/impressum/",
       ],
     },
