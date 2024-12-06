39963e9fd3a689bc4479396d421841ff305ab87c
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2024-12-06 16:53:11 +0100

    Allow sitemap xml

--- app/robots.ts
+++ app/robots.ts
@@ -4,13 +4,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/home/",
-      disallow: [
-        "/",
-        "/administration/",
-        "/home/datenschutz/",
-        "/home/impressum/",
-      ],
+      allow: ["/home", "/sitemap.xml"],
+      disallow: ["/administration/", "/home/datenschutz/", "/home/impressum/"],
     },
     sitemap: "https://www.psychotherapie-seliger.de/sitemap.xml",
   };
