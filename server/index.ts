1da3bb11d2e081afb65ea94db022fa80166256f6
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2025-02-14 13:52:37 +0100

    Restructure server folder

--- server/index.ts
+++ server/index.ts
@@ -1,35 +1,11 @@
-import { OAuth2Client } from "google-auth-library";
-import { people_v1 } from "@googleapis/people";
-import { drive_v3 } from "@googleapis/drive";
-import { ServicesRepository } from "../repositories/services";
-import { InvoicesRepository } from "../repositories/invoices";
-import PatientsRepository from "../repositories/patients";
-import { getToken } from "./getToken";
 import { getUser } from "./getUser";
+import { getPatientsRepository } from "./getPatientsRepository";
+import { getInvoicesRepository } from "./getInvoicesRepository";
+import { getServicesRepository } from "./getServiceRepository";
 
-export { getUser };
-
-export const getAuthClient = async () => {
-  const token = await getToken();
-  const oAuthClient = new OAuth2Client();
-  oAuthClient.setCredentials({
-    access_token: token,
-  });
-  return oAuthClient;
+export {
+  getUser,
+  getPatientsRepository,
+  getServicesRepository,
+  getInvoicesRepository,
 };
-
-export async function getPatientsRepository(): Promise<PatientsRepository> {
-  const auth = await getAuthClient();
-  const peopleClient = new people_v1.People({ auth });
-  return new PatientsRepository(peopleClient);
-}
-
-export async function getInvoicesRepository(): Promise<InvoicesRepository> {
-  const auth = await getAuthClient();
-  const drive = new drive_v3.Drive({ auth });
-  return new InvoicesRepository(drive);
-}
-
-export function getServicesRepository(): ServicesRepository {
-  return new ServicesRepository();
-}
