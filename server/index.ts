801acac3ddacbc7695234f47e16c16d1dec4ca1c
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2025-02-15 08:01:17 +0100

    Add get auth client

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,12 @@ import { getUser } from "./getUser";
 import { getPatientsRepository } from "./getPatientsRepository";
 import { getInvoicesRepository } from "./getInvoicesRepository";
 import { getServicesRepository } from "./getServiceRepository";
+import { getAuthClient } from "./getAuthClient";
 
 export {
   getUser,
   getPatientsRepository,
   getServicesRepository,
   getInvoicesRepository,
+  getAuthClient,
 };
