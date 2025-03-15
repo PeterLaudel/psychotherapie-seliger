8ad45f1877d05743b0f6b4bde7117ab080aff23a
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2025-03-15 13:08:59 +0100

    Fix build step

--- src/environment.ts
+++ src/environment.ts
@@ -2,12 +2,6 @@ import * as dotenv from "dotenv";
 
 dotenv.config({ path: [`.env.${process.env.NODE_ENV}`] });
 
-function getOrCrash(key: string) {
-  const result = process.env[key];
-  if (!result) throw new Error(`${key} environment variable missing`);
-  return result;
-}
-
 export function postgresUrl(): string {
-  return getOrCrash("POSTGRES_URL");
+  return process.env.POSTGRES_URL || "";
 }
