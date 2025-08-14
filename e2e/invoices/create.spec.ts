0c3f8bf8098dc82c41605e1699614795fb648f28
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2025-08-14 11:42:03 +0200

    Add test for invoice creation

--- e2e/invoices/create.spec.ts
+++ e2e/invoices/create.spec.ts
@@ -1,3 +1,4 @@
+import { serviceFactory } from "../../factories/service";
 import { test, expect } from "../fixtures";
 import { patientFactory } from "factories/patient";
 
@@ -6,14 +7,32 @@ test("createas an invoice", async ({ page }) => {
     name: "Max",
     surname: "Mustermann",
   });
+  const service = await serviceFactory.create({
+    short: "Super short",
+  });
+
   await page.goto("/administration/invoices");
 
   await page.getByRole("button", { name: "Rechnung anlegen" }).click();
 
-  await expect(
-    page.getByRole("heading", { name: "Rechnung erstellen" })
-  ).toBeVisible();
+  const heading = page.getByRole("heading", { name: "Rechnung erstellen" });
+  await expect(heading).toBeVisible();
 
   await page.getByRole("combobox", { name: "Patient" }).first().click();
   await page.getByRole("option", { name: patient.name }).click();
+
+  await page.getByRole("textbox", { name: "Diagnose" }).fill("Doof");
+
+  const dateGroup = page.getByRole("group", { name: "Leistungs Datum" });
+  await dateGroup.getByRole("spinbutton", { name: "Tag" }).fill("10");
+  await dateGroup.getByRole("spinbutton", { name: "Monat" }).fill("10");
+  await dateGroup.getByRole("spinbutton", { name: "Jahr" }).fill("2024");
+
+  await page.getByRole("combobox", { name: "Leistung" }).click();
+  await page.getByRole("option", { name: service.short }).click();
+
+  await page.getByRole("button", { name: "Rechnung versenden" }).click();
+
+  const succesAlert = page.getByRole("alert");
+  await expect(succesAlert.getByText("Rechnung wurde erstellt")).toBeVisible();
 });
