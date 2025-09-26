b2e33a58a30c1b962bf9e52252b5de70f93f22bb
Peter Laudel <peter.laudel@gmail.com>
2025-09-26 11:06:37 +0200

    Add qrcode

--- src/app/administration/invoices/create/_hooks/invoiceTemplate.tsx
+++ src/app/administration/invoices/create/_hooks/invoiceTemplate.tsx
@@ -10,6 +10,7 @@ import { createTw } from "react-pdf-tailwind";
 import logoBuffer from "./logo";
 import { Service } from "@/models/service";
 import { BillingInfo, Patient } from "@/models/patient";
+import { generateSepaQrBase64Png } from "./generateSepaQrBase64Png";
 
 type InvoicePatient = Pick<Patient, "name" | "surname" | "birthdate">;
 
@@ -17,12 +18,23 @@ export type Position = {
   serviceDate: string, amount: number, factor: string, service: Service; price: number, pageBreak?: boolean
 };
 
+export interface Recipient {
+  name: string;
+  iban: string;
+  bic?: string;
+  street: string;
+  zip: string;
+  city: string;
+  taxId: string;
+}
+
 export interface Props {
   invoiceNumber: string;
   diagnosis?: string;
   billingInfo?: BillingInfo;
   patient?: InvoicePatient;
   positions: Position[];
+  recepient: Recipient;
 }
 
 function toKey({ service, serviceDate, amount, factor }: Position) {
@@ -35,6 +47,7 @@ export function InvoiceTemplate({
   patient,
   positions,
   diagnosis,
+  recepient,
 }: Props) {
   const tw = createTw({});
   const total = positions.reduce((acc, position) => acc + position.price, 0);
@@ -48,6 +61,15 @@ export function InvoiceTemplate({
     currency: "EUR",
   });
 
+  const data = generateSepaQrBase64Png({
+    recipient: recepient.name,
+    bic: recepient.bic,
+    iban: recepient.iban,
+    amount: total,
+    purposeCode: "MEDI",
+    remittanceInfo: invoiceNumber,
+  })
+
   return (
     <Document>
       <Page size="A4" style={tw("p-12")} wrap>
@@ -58,7 +80,7 @@ export function InvoiceTemplate({
           <View style={tw("flex-row")}>
             <View>
               <Text style={tw("text-xs border-b")}>
-                Ute Seliger - Friedrich-Ebert-Straße 98 - 04105 Leipzig
+                {`${recepient.name} • ${recepient.street} • ${recepient.zip} ${recepient.city}`}
               </Text>
               {billingInfo && (
                 <>
@@ -79,8 +101,8 @@ export function InvoiceTemplate({
             <Text style={tw("font-bold")}>M.Sc. A.Ute Seliger</Text>
             <Text>Psychologische Psychotherapeutin</Text>
             <Text style={tw("mb-3")}>ENR.: 9660750</Text>
-            <Text>Friedrich-Ebert-Str. 98</Text>
-            <Text style={tw("mb-3")}>04105 Leipzig</Text>
+            <Text>{`${recepient.street}`}</Text>
+            <Text style={tw("mb-3")}>{`${recepient.zip} ${recepient.city}`}</Text>
             <Text>psychotherapie@praxis-seliger.com</Text>
           </View>
         </View>
@@ -204,16 +226,25 @@ export function InvoiceTemplate({
             Verwendungszweck die Rechnungsnummer an.
           </Text>
         </View>
+        <View style={tw("flex-row h-[8vh] justify-end mt-4")}>
+          <View style={tw("flex-row border")}>
+            <View style={tw("flex-col items-center self-center ml-2")}>
+              <Text style={tw("text-sm font-bold")}>Überweisen per Code</Text>
+              <Text style={tw("text-sm")}>Code mit der Banking-App scannen</Text>
+            </View>
+            <ReactPdfImage src={data} />
+          </View>
+        </View>
         <View style={tw("absolute bottom-8 self-center")} fixed>
           <View style={tw("flex-col items-center")}>
             <Text style={tw("text-sm")}>Deutsche Apotheker und Ärztebank</Text>
             <Text style={tw("text-sm")}>
-              IBAN: DE16 3006 0601 0022 1680 56 • Swift/BIC: DAAEDEDDXXX
+              {`IBAN: ${recepient.iban} • Swift/BIC: ${recepient.bic}`}
             </Text>
-            <Text style={tw("text-sm")}>Steuernummer: 231/274/03511</Text>
+            <Text style={tw("text-sm")}>{`Steuernummer: ${recepient.taxId}`}</Text>
           </View>
         </View>
       </Page>
-    </Document>
+    </Document >
   );
 }
