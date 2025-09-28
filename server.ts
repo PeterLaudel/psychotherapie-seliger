134d1f47809eb0197ae76002b7d9253e7402e023
Peter Laudel <peter.laudel@gmail.com>
2025-09-29 00:20:41 +0200

    WIP

---
+++ server.ts
@@ -0,0 +1,22 @@
+import next from 'next';
+import * as http from 'http';
+
+import { dbMigrate } from './tasks/dbMigrate';
+import { dbSeed } from './tasks/dbSeed';
+
+dbMigrate();
+dbSeed();
+
+const dev = false; // we always run production mode
+const app = next({ dev, dir: __dirname });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  http
+    .createServer((req, res) => {
+      handle(req, res);
+    })
+    .listen(3000, () => {
+      console.log("🚀 Next.js server running on http://localhost:3000");
+    });
+});
