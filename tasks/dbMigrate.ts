da69ca4aff031c75671fe443dc303be2756ac835
Peter Laudel <peter.laudel@gmail.com>
2025-09-29 00:20:41 +0200

    WIP

---
+++ tasks/dbMigrate.ts
@@ -0,0 +1,33 @@
+import { FileMigrationProvider, Migrator } from "kysely";
+import { promises as fs } from 'fs'
+import { db } from '../src/initialize'
+import * as path from 'path'
+
+export function dbMigrate() {
+    const migration = new Migrator({
+        db,
+        provider: new FileMigrationProvider({
+            fs,
+            path,
+            migrationFolder: path.join(__dirname, '/../migrations'),
+        })
+    })
+
+    // migration.migrateToLatest();
+    migration.migrateToLatest().then(({ error, results }) => {
+        results?.forEach((it) => {
+            if (it.status === 'Success') {
+                console.log(`migration "${it.migrationName}" was executed successfully`)
+            } else if (it.status === 'Error') {
+                console.error(`failed to execute migration "${it.migrationName}"`)
+            }
+        })
+
+        if (error) {
+            console.error('failed to migrate')
+            console.error(error)
+            process.exit(1)
+        }
+    })
+}
+
