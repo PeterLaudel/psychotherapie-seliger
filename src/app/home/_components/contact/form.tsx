82a284e9f5cd33a23f498e63419e084d3250d736
Peter Laudel <peter.laudel@gmail.com>
2025-11-04 11:33:59 +0100

    Simplify form

--- src/app/home/_components/contact/form.tsx
+++ src/app/home/_components/contact/form.tsx
@@ -1,16 +1,14 @@
 "use client";
 
-import { FORM_ERROR } from "final-form";
-import { ReactNode, useMemo } from "react";
-import { Field, Form as FinalForm } from "react-final-form";
-import Spinner from "./spinner";
 import {
-  validateEmail,
-  validateMessage,
-  validateName,
-  validatePayment,
-  validateSurname,
-} from "./validations";
+  FormEvent,
+  ReactNode,
+  startTransition,
+  useActionState,
+  useState,
+} from "react";
+import Spinner from "./spinner";
+import { validateContactForm } from "./validation";
 
 interface ContactFormValues {
   name?: string;
@@ -26,15 +24,23 @@ function Error({ children }: { children: ReactNode }) {
   return <div className="text-red-500 text-sm">{children}</div>;
 }
 
-export default function Form() {
-  const initialValues: ContactFormValues = useMemo(
-    () => ({
-      payment: "",
-    }),
-    []
-  );
+type ActionState = null | {
+  success: boolean;
+};
 
-  const onSubmit = async (values: ContactFormValues) => {
+export default function Form() {
+  const [state, action, pending] = useActionState(formAction, null);
+  const [validationErrors, setValidationErrors] = useState<
+    Partial<ContactFormValues> | undefined
+  >(undefined);
+
+  async function formAction(state: ActionState, formData: FormData) {
+    const values = Object.fromEntries(formData.entries()) as ContactFormValues;
+    const validationErrors = validateContactForm(formData);
+    if (validationErrors) {
+      setValidationErrors(validationErrors);
+      return { success: false };
+    }
     const searchParams = new URLSearchParams({
       action: "contactRequest",
     }).toString();
@@ -43,158 +49,109 @@ export default function Form() {
       body: JSON.stringify(values),
     });
 
-    if (response.ok) return;
+    if (response.ok) return { success: true };
 
     return {
-      [FORM_ERROR]: "Ein Fehler ist aufgetreten",
+      success: false,
     };
-  };
+  }
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    setValidationErrors(undefined);
+    e.preventDefault();
+    startTransition(() => {
+      action(new FormData(e.currentTarget));
+    });
+  }
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <FinalForm<ContactFormValues>
-        onSubmit={onSubmit}
-        initialValues={initialValues}
-      >
-        {({
-          hasValidationErrors,
-          submitFailed,
-          handleSubmit,
-          submitSucceeded,
-          submitting,
-        }) => (
-          <form onSubmit={handleSubmit} className="w-full max-w-md">
-            <h1 className="text-2xl font-bold">Termin anfragen</h1>
-            <h2 className="text-lg font-bold my-3 text-red-600">
-              Freie Termine verfübar ab Oktober 2025.
-            </h2>
-            <div className="my-5">
-              Bitte beschreiben Sie kurz Ihr Anliegen. Geben Sie uns bitte
-              außerdem an, in welchen Zeiträumen Therapiesitzungen stattfinden
-              könnten.
+      <form onSubmit={handleSubmit} className="w-full max-w-md grid gap-4">
+        <h1 className="text-2xl font-bold">Termin anfragen</h1>
+        <div className="my-1">
+          Bitte beschreiben Sie kurz Ihr Anliegen. Geben Sie uns bitte außerdem
+          an, in welchen Zeiträumen Therapiesitzungen stattfinden könnten.
+        </div>
+
+        <label className="grid">
+          <div className="font-bold text-lg">Vorname</div>
+          <input
+            name="name"
+            className="border-2 border-gray-300 p-2 rounded-md"
+          />
+          {validationErrors?.name && <Error>{validationErrors.name}</Error>}
+        </label>
+
+        <label className="grid">
+          <div className="font-bold text-lg">Nachname</div>
+          <input
+            name="surname"
+            className="border-2 border-gray-300 p-2 rounded-md"
+          />
+          {validationErrors?.surname && (
+            <Error>{validationErrors.surname}</Error>
+          )}
+        </label>
+
+        <label className="grid">
+          <div className="font-bold text-lg">Email</div>
+          <input name="email" className="border-2 p-2 rounded-md" />
+          {validationErrors?.email && <Error>{validationErrors.email}</Error>}
+        </label>
+
+        <label className="grid">
+          <div className="text-lg font-bold">Nachricht</div>
+          <textarea
+            name="message"
+            placeholder="Bitte beschreiben Sie Ihr Anliegen und nennen Sie Ihre Verfügbarkeit"
+            className="border-2 p-2 rounded-md"
+          />
+          {validationErrors?.message && (
+            <Error>{validationErrors.message}</Error>
+          )}
+        </label>
+
+        <label className="grid">
+          <div className="text-lg font-bold">Bezahlung</div>
+          <select
+            name="payment"
+            className="border-2 p-2 rounded-md bg-white"
+            defaultValue=""
+          >
+            <option hidden value=""></option>
+            <option value="privatversichert">Privatversichert</option>
+            <option value="beihilfe">Beihilfe</option>
+            <option value="heilfürsorge">Heilfürsorge</option>
+            <option value="selbstzahler">Selbstzahler</option>
+          </select>
+          {validationErrors?.payment && (
+            <Error>{validationErrors.payment}</Error>
+          )}
+        </label>
+
+        <div className="grid">
+          <button
+            type="submit"
+            disabled={pending || state?.success}
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md disabled:opacity-50"
+          >
+            {pending && <Spinner />}
+            Absenden
+          </button>
+          {state?.success && (
+            <div className="text-green-500">
+              Vielen Dank für Ihre Anfrage. Wir werden uns in Kürze bei Ihnen
+              melden.
             </div>
-            <Field name="name" validate={validateName} type="text">
-              {({ input, meta: { touched, error } }) => (
-                <div className="flex flex-col mb-4">
-                  <label
-                    htmlFor={input.name}
-                    className="mb-2 font-bold text-lg text-gray-900"
-                  >
-                    Vorname
-                  </label>
-                  <input
-                    {...input}
-                    className="border-2 border-gray-300 p-2 rounded-md"
-                  />
-                  {touched && error && <Error>{error}</Error>}
-                </div>
-              )}
-            </Field>
-
-            <Field name="surname" validate={validateSurname} type="text">
-              {({ input, meta: { touched, error } }) => (
-                <div className="flex flex-col mb-4">
-                  <label
-                    htmlFor={input.name}
-                    className="mb-2 font-bold text-lg text-gray-900"
-                  >
-                    Nachname
-                  </label>
-                  <input
-                    {...input}
-                    className="border-2 border-gray-300 p-2 rounded-md"
-                  />
-                  {touched && error && <Error>{error}</Error>}
-                </div>
-              )}
-            </Field>
-
-            <Field name="email" validate={validateEmail} type="text">
-              {({ input, meta: { touched, error } }) => (
-                <div className="flex flex-col mb-4">
-                  <label
-                    htmlFor={input.name}
-                    className="mb-2 font-bold text-lg text-gray-900"
-                  >
-                    Email
-                  </label>
-                  <input
-                    {...input}
-                    className="border-2 border-gray-300 p-2 rounded-md"
-                  />
-                  {touched && error && <Error>{error}</Error>}
-                </div>
-              )}
-            </Field>
-
-            <Field name="message" validate={validateMessage} type="textarea">
-              {({ input, meta: { touched, error } }) => (
-                <div className="flex flex-col mb-4">
-                  <label
-                    htmlFor={input.name}
-                    className="mb-2 font-bold text-lg text-gray-900"
-                  >
-                    Nachricht
-                  </label>
-                  <textarea
-                    {...input}
-                    placeholder="Bitte beschreiben Sie Ihr Anliegen und nennen Sie Ihre Verfügbarkeit"
-                    className="border-2 border-gray-300 p-2 rounded-md"
-                  />
-                  {touched && error && <Error>{error}</Error>}
-                </div>
-              )}
-            </Field>
-
-            <Field name="payment" type="select" validate={validatePayment}>
-              {({ input, meta: { touched, error } }) => (
-                <div className="flex flex-col mb-4">
-                  <label
-                    htmlFor={input.name}
-                    className="mb-2 font-bold text-lg text-gray-900"
-                  >
-                    Bezahlung
-                  </label>
-                  <select
-                    {...input}
-                    className="border-2 border-gray-300 p-2 rounded-md bg-white"
-                  >
-                    <option disabled value=""></option>
-                    <option value="privatversichert">Privatversichert</option>
-                    <option value="beihilfe">Beihilfe</option>
-                    <option value="heilfürsorge">Heilfürsorge</option>
-                    <option value="selbstzahler">Selbstzahler</option>
-                  </select>
-                  {touched && error && <Error>{error}</Error>}
-                </div>
-              )}
-            </Field>
-
-            <div className="flex flex-col">
-              <button
-                type="submit"
-                disabled={submitting || submitSucceeded}
-                className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md disabled:opacity-50"
-              >
-                {submitting && <Spinner />}
-                Absenden
-              </button>
-              {submitSucceeded && (
-                <div className="text-green-500">
-                  Vielen Dank für Ihre Anfrage. Wir werden uns in Kürze bei
-                  Ihnen melden.
-                </div>
-              )}
-              {hasValidationErrors && submitFailed && (
-                <Error>{"Formular unvollständig"}</Error>
-              )}
-              {!hasValidationErrors && submitFailed && (
-                <Error>{"Ein Fehler ist aufgetreten"}</Error>
-              )}
-            </div>
-          </form>
-        )}
-      </FinalForm>
+          )}
+          {validationErrors !== undefined && (
+            <Error>{"Formular unvollständig"}</Error>
+          )}
+          {validationErrors === undefined && state && !state.success && (
+            <Error>{"Ein Fehler ist aufgetreten"}</Error>
+          )}
+        </div>
+      </form>
     </div>
   );
 }
