c8d00215b2fda6dff16556d33ee3c58600b01e61
Peter Laudel <peter.laudel@gmail.com>
2026-05-07 20:51:20 +0200

    Run all tests

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,14 @@ set -e
 
 if [ "$1" = "test" ]; then
   export NODE_ENV=test
-  npm run db:create
-  npm run db:migrate
-  exec npm run test:ci
+  DATABASE_DIALECT=postgres npm run db:create
+  DATABASE_DIALECT=postgres npm run db:migrate
+  npm run test:postgres
+  DATABASE_DIALECT=sqlite npm run db:create
+  DATABASE_DIALECT=sqlite npm run db:migrate
+  npm run test:sqlite
+  npm run lint
+  exec npm run typecheck
 elif [ "$1" = "e2e" ]; then
   NODE_ENV=e2e npm run db:create
   NODE_ENV=e2e npm run db:migrate
