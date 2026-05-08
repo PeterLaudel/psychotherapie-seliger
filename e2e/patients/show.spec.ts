853725ccc03f2f0227ff14a2b2c3f6ff27d2e083
Peter Laudel <peter.laudel@gmail.com>
2026-05-08 22:21:18 +0200

    Add filter patients test

---
+++ e2e/patients/show.spec.ts
@@ -0,0 +1,19 @@
+import { patientFactory } from "factories/patient";
+import { test, expect } from "../fixtures";
+
+test("filter patients", async ({ page }) => {
+  await patientFactory.create({ name: "Jane", surname: "Doe" });
+  await patientFactory.create({ name: "John", surname: "Doe" });
+
+  await page.goto("/administration/patients");
+
+  await expect(page.getByText("Jane")).toBeVisible();
+  await expect(page.getByText("John")).toBeVisible();
+
+  await page.getByPlaceholder("Suche").fill("Jane");
+
+  await page.waitForResponse("/api/patients*");
+
+  await expect(page.getByText("John")).not.toBeVisible();
+  await expect(page.getByText("Jane")).toBeVisible();
+});
